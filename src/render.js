11ae458d6049228d83053fba819978e000ff7842
bruno-rodrigues-d <brunorodrigues@gec.inatel.br>
2023-08-15 09:56:52 -0300

    feat: When clicked redirect to video minutes

--- src/render.js
+++ src/render.js
@@ -8,11 +8,35 @@ const renderChunk = ({timestamp, text}) => `
 `
 
 function getMinutes(timestamp) {
-  return '00:00'
+  let date = new Date(null)
+  date.setTime(timestamp[0] * 1000);
+
+  return date.toISOString().slice(14, 19);
+}
+
+window.seek = function(event) {
+  const seekTo = event.currentTarget.datset.seekTo;
+  window.YTPlayer.seekTo(seekTo);
+  window.YTPlayer.playVideo();
 }
 
 function groupedText(text, timestamp) {
-  return text;
+  const words = text.split(' ');
+  const groups = [];
+
+  for (let idx = 0; idx < words.length; idx++) {
+    if (idx % 3 === 0) {
+      groups.push(words.slice(idx, idx + 3).join(' '));
+    }
+  }
+
+  return groups.map((item, idx) => {
+    const [initialTime, finalTime] = timestamp
+    const seekTo = idx == 0 ? initialTime :
+      ((finalTime - initialTime) / (groups.length - idx)) + initialTime;
+
+    return `<span onclick=seek(event) data-seek-to=${seekTo}>${item} </span>`
+  }).join('');
 }
 
 export function renderText({chunks}) {
